e32f4ebef7c6e6e8a5bea2fc747b695f531a2971
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-04-04 09:45:17 -0400

    shapes and js files

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const sheapes = require('shapes.js')
+
+const userInput = [
+    { // user input for the SVG text
+        type: 'input',
+        name: 'SVG text',
+        message: 'Enter up to 3 characters for the SVG logo text',
+        validate: function (input){
+            return input.length <=3
+        },
+    },
+    {
+        
+    }
+]

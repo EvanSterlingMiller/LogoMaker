98d91f85a93cd4a87214bc3111a0c8fa0b983797
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-04-04 18:10:23 -0400

    first attempt at the js file structure

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const sheapes = require('shapes.js')
 const userInput = [
     { // user input for the SVG text
         type: 'input',
-        name: 'SVG text',
+        name: 'text',
         message: 'Enter up to 3 characters for the SVG logo text',
         validate: function (input){
             return input.length <=3
         },
     },
-    {
-        
-    }
+    {//  user input of the text color
+       type: 'input',
+       name:  'colorText',
+       message: 'What color do you want your logo text?',
+    },
+    {// user input for the shape of the logo
+        type: 'list',
+        name: 'shape',
+        message: 'Select what shape you want your logo to be:',
+        choices: ['triangle', 'circle', 'square']
+    },
+    {// user color selection
+        type: 'input',
+        name: 'colorShape',
+        message: 'What color do you want your logo shape to be?',
+    },
 ]
+
+
